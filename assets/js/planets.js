b6a77595838cdac9b9eb93f7838e393c6104e018
Jomar Yockey <j_yockey@icloud.com>
2019-04-24 01:07:08 -0500

    updated scene

--- assets/js/planets.js
+++ assets/js/planets.js
@@ -1,15 +1,28 @@
 // DECLARE VARIABLES AND FUNCTIONS
 let scene, camera, renderer;
 
-let sphere, torusA, torusB, torusC;
+let ico1;
 let planet;
+let donuts = [];
 let rings = [];
-let delta = 0.05;
 
+let delta = 0.05;
+let theta = 0.1;
+let gamma = 0.1;
+let beta = 0.001;
+
+let createIco = function() {
+    let geometryA = new THREE.IcosahedronGeometry(50, 6);
+    let geometryB = new THREE.IcosahedronGeometry(30, 5);
+    let material = new THREE.MeshNormalMaterial({wireframe: true});
+    ico1 = new THREE.Mesh(geometryA, material);
+    ico2 = new THREE.Mesh(geometryB, material);
+    scene.add(ico1, ico2);
+}
 
 let createSaturn = function() {
     let geometry = new THREE.SphereGeometry(4, 30, 30);
-    let material = new THREE.MeshNormalMaterial({wireframe: true});
+    let material = new THREE.MeshNormalMaterial();
     planet = new THREE.Mesh(geometry, material);
     scene.add(planet);
 
@@ -32,22 +45,22 @@ let createSaturn = function() {
     });
 }
 
-let createTorus = function() {
-    let geometryA = new THREE.TorusGeometry(2.5, 0.5, 2, 200);
-    let geometryB = new THREE.TorusGeometry(5, 0.5, 2, 200);
-    let geometryC = new THREE.TorusGeometry(7, 0.5, 2, 200);
-    let material = new THREE.MeshNormalMaterial();
-    torusA = new THREE.Mesh(geometryA, material);
-    torusB = new THREE.Mesh(geometryB, material);
-    torusC = new THREE.Mesh(geometryC, material);
-    scene.add(torusA, torusB, torusC);
+let randomRange = function(from, to) {
+    let x = Math.random() * (to - from);
+    return x + from;
 }
 
-let createSphere = function() {
-    let geometry = new THREE.SphereGeometry(1, 20, 20);
-    let material = new THREE.MeshNormalMaterial();
-    sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
+let createDonut = function () {
+    let geometry = new THREE.TorusKnotGeometry(2, 1, 20, 100);
+    let material = new THREE.MeshNormalMaterial({wireframe: true});
+    torus = new THREE.Mesh(geometry, material);
+
+    torus.position.x = randomRange(-50, 50);
+    torus.position.y = 30;
+    torus.position.z = randomRange(-50, -30);
+
+    scene.add(torus);
+    donuts.push(torus);
 }
 
 // INITIALIZE THE SCENE
@@ -66,8 +79,7 @@ let init = function() {
     camera.position.z = 50;
 
     // call polygon functions
-    // createSphere();
-    // createTorus();
+    createIco();
     createSaturn();
 
     // create and set the size of the renderer
@@ -80,21 +92,15 @@ let init = function() {
 
 // MAIN LOOP
 let loop = function() {
-    // sphere.rotation.y += 0.01;
-    // sphere.rotation.z += 0.01;
+    ico1.position.y += delta;
+    ico1.rotation.z += beta;
 
-    // torusA.rotation.x += -0.01;
-    // torusA.rotation.y += -0.03;
-
-    // torusB.rotation.x += 0.01;
-    // torusB.rotation.y += 0.02;
-
-    // torusC.rotation.x += -0.01;
-    // torusC.rotation.y += -0.04;
+    ico2.rotation.y += beta;
+    ico2.rotation.z -= beta;
 
     planet.position.x += -delta;
     planet.position.y += -delta;
-    // planet.position.z += -delta;
+    
 
     planet.rotation.x += 0.01;
     planet.rotation.y += 0.02;
@@ -127,6 +133,22 @@ let loop = function() {
     if(camera.position.y >= 5 || camera.position.y <= -5) {
         delta *= -1;
     }
+    
+    let random = Math.random();
+    if(random < 0.05) {
+        createDonut();
+    }
+
+    donuts.forEach(torus => {
+        torus.position.y -= theta;
+        torus.position.x += gamma;
+        if(torus.position.x >= 15 || torus.position.x <= -5) {
+            gamma *= -1;
+        }
+        torus.rotation.x += theta;
+        torus.rotation.y += theta;
+        torus.rotation.z += delta;
+    });
 
     renderer.render(scene, camera);
     requestAnimationFrame(loop);
