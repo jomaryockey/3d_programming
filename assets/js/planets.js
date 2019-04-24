e32f0ab9497b068ab53a2df7a5880a53de5bd144
Jomar Yockey <j_yockey@icloud.com>
2019-04-23 23:27:53 -0500

    updated scene

--- assets/js/planets.js
+++ assets/js/planets.js
@@ -2,6 +2,35 @@
 let scene, camera, renderer;
 
 let sphere, torusA, torusB, torusC;
+let planet;
+let rings = [];
+let delta = 0.05;
+
+
+let createSaturn = function() {
+    let geometry = new THREE.SphereGeometry(4, 30, 30);
+    let material = new THREE.MeshNormalMaterial({wireframe: true});
+    planet = new THREE.Mesh(geometry, material);
+    scene.add(planet);
+
+    geometry = new THREE.TorusGeometry(5.1, 0.7, 2, 500);
+    let ring = new THREE.Mesh(geometry, material);
+    rings.push(ring);
+
+    geometry = new THREE.TorusGeometry(6.9, 0.7, 2, 500);
+    ring = new THREE.Mesh(geometry, material);
+    rings.push(ring);
+
+    geometry = new THREE.TorusGeometry(8.5, 0.7, 2, 500);
+    ring = new THREE.Mesh(geometry, material);
+    rings.push(ring);
+
+    rings.forEach(ring => {
+        ring.rotation.x = 1.7;
+        ring.rotation.y = 0.5;
+        scene.add(ring);
+    });
+}
 
 let createTorus = function() {
     let geometryA = new THREE.TorusGeometry(2.5, 0.5, 2, 200);
@@ -37,8 +66,9 @@ let init = function() {
     camera.position.z = 50;
 
     // call polygon functions
-    createSphere();
-    createTorus();
+    // createSphere();
+    // createTorus();
+    createSaturn();
 
     // create and set the size of the renderer
     renderer = new THREE.WebGLRenderer();
@@ -50,17 +80,53 @@ let init = function() {
 
 // MAIN LOOP
 let loop = function() {
-    sphere.rotation.y += 0.01;
-    sphere.rotation.z += 0.01;
+    // sphere.rotation.y += 0.01;
+    // sphere.rotation.z += 0.01;
 
-    torusA.rotation.x += -0.01;
-    torusA.rotation.y += -0.03;
+    // torusA.rotation.x += -0.01;
+    // torusA.rotation.y += -0.03;
 
-    torusB.rotation.x += 0.01;
-    torusB.rotation.y += 0.02;
+    // torusB.rotation.x += 0.01;
+    // torusB.rotation.y += 0.02;
 
-    torusC.rotation.x += -0.01;
-    torusC.rotation.y += -0.04;
+    // torusC.rotation.x += -0.01;
+    // torusC.rotation.y += -0.04;
+
+    planet.position.x += -delta;
+    planet.position.y += -delta;
+    // planet.position.z += -delta;
+
+    planet.rotation.x += 0.01;
+    planet.rotation.y += 0.02;
+    planet.rotation.z += 0.01;
+
+    rings.forEach(ring => {
+        ring.rotation.x += 0.05;
+        ring.rotation.y += -0.02;
+        ring.rotation.z += -0.05;
+    });
+
+    rings[0].rotation.x += -0.02;
+    rings[0].rotation.y += -0.02;
+    rings[0].rotation.z += -0.02;
+
+    rings[0].position.y += delta;
+    rings[0].position.x += delta;
+    rings[0].position.z += delta;
+
+    rings[1].position.y += 1.2 * -delta;
+    rings[1].position.x += 1.2 * -delta;
+    rings[1].position.z += 1.2 * -delta;
+
+    rings[2].position.y += 1.2 * delta;
+    rings[2].position.x += 1.2 * delta;
+    rings[2].position.z += 1.2 * delta;
+
+
+    camera.position.y += delta;
+    if(camera.position.y >= 5 || camera.position.y <= -5) {
+        delta *= -1;
+    }
 
     renderer.render(scene, camera);
     requestAnimationFrame(loop);
